b3e1bed2fef00d2b9f093965d970e7db42669f37
rksksl <bbum__@naver.com>
2021-11-08 18:53:43 +0900

    update

--- 13주차 스터디자료/난이도 중/큐브 미로.py
+++ 13주차 스터디자료/난이도 중/큐브 미로.py
@@ -1,18 +1,34 @@
 from sys import stdin
 
-l, r, c = map(int, stdin.readline().split())
+def escape_cube():
+    dx = [-1, 1, 0, 0, 0, 0]
+    dy = [0, 0, -1, 1, 0, 0]
+    dz = [0, 0, 0, 0, -1, 1]
 
-cube = []
+def main():
+    l, r, c = map(int, stdin.readline().split())
 
-for i in range(c):
-    maze = []
-    for j in range(r + 1):
-        m = list(map(str, stdin.readline().split()))
-        if not m:
-            continue
-        elif '0' in m:
-            break
-        else:
-            maze.append(m)
-    cube.append(maze)
+    cube = []
+    start = []
+    end = []
 
+    for i in range(c):
+        maze = []
+        for j in range(r + 1):
+            m = list(map(str, stdin.readline().split()))
+            if not m:
+                continue
+            elif '0' in m:
+                break
+            else:
+                maze.append(m)
+                if m.find('S') != -1 and not start:
+                    start = [m.find('S'), j, i]
+                elif m.find('E') != -1 and not end:
+                    end = [m.find('E', j, i)]
+        cube.append(maze)
+
+
+    
+if __name__ == '__main__':
+    main()
