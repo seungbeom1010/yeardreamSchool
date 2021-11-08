505178963ccb13f64ff7db453caf2630dda9c8e9
seungbeom1010 <seungbeom.dev@gmail.com>
2021-11-08 20:53:55 +0900

    update

--- 13주차 스터디자료/난이도 하/앞면 뒷면.py
+++ 13주차 스터디자료/난이도 하/앞면 뒷면.py
@@ -1,15 +1,7 @@
-import sys
-n = int(sys.stdin.readline())
+import math
 
-#80점 수정필요
+n = int(input())
+a = math.sqrt(n)
+result = a // 1
 
-cards = [0] * n
-
-for i in range(n):
-    for j in range(n):
-        if (j + 1) % (i + 1) == 0 and cards[j] == 0:
-            cards[j] = 1
-        elif (j + 1) % (i + 1) == 0 and cards[j] == 1:
-            cards[j] = 0
-
-print(cards.count(1))
+print(int(result))
--- 13주차 스터디자료/난이도 하/재벌의 쇼핑.py
+++ 13주차 스터디자료/난이도 하/재벌의 쇼핑.py
@@ -1,19 +1,33 @@
-import sys
-import math
+from sys import stdin
 
-#60점 수정 필요
+def shopping(products, n, s):
 
-n, s = map(int, sys.stdin.readline().split())
-goods = list(map(int, sys.stdin.readline().split()))
+    count = []  # 최소금액이 맞춰질때, 상품의 개수
+    for i in range(n): # 시작점
+        clothes = 1
+        sum_money = [] # 구간합을 저장하는 리스트, 시작점이 달라질 때마다 reset
+        sum_money.append(products[i])
+        if sum_money[-1] >= s:
+            count.append(clothes)
+        for j in range(i+1,n):
+            t = sum_money[-1] + products[j]
+            clothes += 1
+            if t < s:
+                sum_money.append(t)
+            else:
+                count.append(clothes)
+                break
 
-result = math.inf
+    if len(count) == 0:
+        return 0
+    return(min(count))
 
-if sum(goods) < s:
-    print(0)
 
-else:
-    for i in range(n):
-        sum_value = 0
-        result = min(result, len(goods[i:]))
-    
-    print(result)
+def main():
+    n,s = map(int, input().split())  # n=상품의 개수, s = 쇼핑에 사용할 최소금액
+    products = list(map(int, stdin.readline().split()))  # 상품리스트
+    print(shopping(products, n, s))
+
+
+if __name__ == "__main__":
+    main()
